c76d83982c4ff415eb6693c7dd53f21369b3f92f
AlexRoy2006 <alexroy2028@cs.sjcetpalai.ac.in>
2025-07-20 22:55:43 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,23 @@
-     // TODO: Create a class called Student with name, rollNumber, and grade
+  public class Student{
+        String name;
+        int rollNumber;
+        char grade;
+        
+        void dispalyDetails(){
+            System.out.println("Name: "+name);
+            System.out.println("Roll Number:"+rollNumber);
+            System.out.println("Grade:"+grade);
+        }
+    }       // TODO: Create a class called Student with name, rollNumber, and grade
 
     // TODO: Create a method to display student details
 public class Main {
       public static void main(String[] args) {
+          Student si = new Student();
+          si.name="Alex";
+          si.rollNumber=12;
+          si.grade='A';
+          si.dispalyDetails();
         // TODO: Create a Student object and call the method to display details
     }
 }
